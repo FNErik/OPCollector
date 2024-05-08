a8cd2eb8809c8d088e784236beb6fa0361180a93
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-05-08 13:40:53 +0200

    Create Home.tsx

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Home = () =>{
+    return (
+        <Fragment>
+            <h1>Soy el home</h1>
+        </Fragment>
+    )
+}
+
+export default Home;
