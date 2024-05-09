ffe0b47fd9f379d7924d2e9f1d17b977567f39e9
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-05-09 14:14:42 +0200

    Login registro ok dashboard creado

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/home/Home.tsx';
+//import Home from './pages/home/Home.tsx';
 import Login from './pages/login/Login.tsx';
 import SignUp from './pages/signup/SignUp.tsx';
+import Dashboard from './pages/admin/dashboard/Dashboard.tsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         {/*<Route path="/" element={<Home />} />*/}
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/admin" element={<Dashboard />} />
       </Routes>
     </Router>
   );
