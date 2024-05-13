3e259a867f471c4386337cc04f2c1792b715dab0
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-05-13 20:46:37 +0200

    Chatgpt cooked

--- frontend/src/components/CollectionCard.tsx
+++ frontend/src/components/CollectionCard.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import './css/CollectionCard.css'
-
+import React, { useState } from 'react';
+import './css/CollectionCard.css';
 interface Props {
     collectionName: string;
 }
@@ -10,19 +9,28 @@ const CollectionCard = ({ collectionName }: Props) => {
         return `../collections/${collectionName}.png`;
     };
 
-    const imageUrl = getImageUrl(collectionName);
+    const [imageUrl, setImageUrl] = useState(getImageUrl(collectionName));
+
+    // Función para manejar errores de carga de la imagen
+    const handleImageError = () => {
+        if (collectionName.startsWith("ST")) {
+            setImageUrl('../collections/ST_DEFAULT.png');
+        } else {
+            setImageUrl('../collections/OP_EB_DEFAULT.png')
+        }
+    };
 
     return (
         <figure className="m-1 card-wrapper">
             <img 
-                className=' object-contain w-full aspect-square card'
+                className='object-contain w-full aspect-square card'
                 src={imageUrl}
                 alt={collectionName}
+                onError={handleImageError} // Maneja el error de carga de la imagen
             />
             <figcaption>{collectionName}</figcaption>
         </figure>
     );
 }
-
 
 export default CollectionCard;
