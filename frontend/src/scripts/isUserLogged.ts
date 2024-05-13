7cb2f058514954e9d24cdfdea7ca0cf8667e747f
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-05-13 10:48:21 +0200

    Update isUserLogged.ts

--- frontend/src/scripts/isUserLogged.ts
+++ frontend/src/scripts/isUserLogged.ts
@@ -1,7 +1,9 @@
+import { User } from "../types/User";
+
 export default function isUserLogged(verbose: boolean = true){
     const storedUser = localStorage.getItem("user");
     if (storedUser !== null) {
-        const user = JSON.parse(storedUser);
+        const user: User = JSON.parse(storedUser);
         if(verbose) {
             console.log("User:");
             console.log(user);
