269787737f6b1cb82d9d67f8dd5af658c9aee11c
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-05-14 18:27:43 +0200

    Fancy input de type

--- frontend/src/pages/browseCards/BrowseCards.tsx
+++ frontend/src/pages/browseCards/BrowseCards.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import Header from '../../components/Header.tsx';
 import { User } from '../../types/User.ts';
 import getCurrentUser from '../../scripts/getCurrentUser.ts';
-import { Input } from '@mui/material';
+import CardTypesAutocomplete from '../../components/CardTypesAutocomplete.tsx';
 
 const BrowseCards = () => {
     const user: User | null = getCurrentUser();
@@ -10,25 +10,11 @@ const BrowseCards = () => {
         <Fragment>
             <Header user={user} />
             <main className='mt-40 px-4 md:px-20 lg:px-40 flex flex-col items-center'>
-                <div className='w-full border-2 border-black'>
+                <div className='w-full'>
                     <h1>Filtros</h1>    
                 </div>
-                <div className='w-full border-2 border-black flex'>
-                    <div className="w-1/5 border-2 border-black h-10">
-                        
-                    </div>
-                    <div className="w-1/5 border-2 border-black h-10">
-                        
-                    </div>
-                    <div className="w-1/5 border-2 border-black h-10">
-                        
-                    </div>
-                    <div className="w-1/5 border-2 border-black h-10">
-                        
-                    </div>
-                    <div className="w-1/5 border-2 border-black h-10">
-                        
-                    </div>
+                <div className='w-full'>
+                        <CardTypesAutocomplete />
                 </div>
             </main>
         </Fragment>
