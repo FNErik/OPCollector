c92f30a3f7cbe1324035e350a3354c45fb581556
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-05-22 17:50:16 +0200

    Pagina principal coquette

--- frontend/src/pages/landing/Landing.tsx
+++ frontend/src/pages/landing/Landing.tsx
@@ -17,13 +17,13 @@ export default function LandingPage() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Header user={user}/>
-        <main className='md:mt-40 mt-20 px-4 md:px-20 lg:px-40 flex flex-col items-center'>
+        <main className='md:mt-30 mt-20 pt-10 px-4 md:px-20 lg:px-40 flex flex-col items-center'>
             <div className='diagonalParent w-full h-80 flex shadow-lg rounded-lg relative'>
                 <div className='banner-text flex flex-col justify-evenly md:justify-start'>
                   <h1 className="
                     text-black font-bold
                     md:mt-6 md:ml-6 lg:mt-8 lg:ml-8 xl:mt-10 xl:ml-10
-                    text-4xl md:text-5xl lg:text-6xl
+                    text-4xl md:text-5xl lg:text-6xl 2xl:text-8xl
                     text-center md:text-left
                   ">
                     JOIN OUR <span className="text-red-500">CREW!</span>
@@ -44,7 +44,19 @@ export default function LandingPage() {
                 </div>
                 <div className='diagonal w-full h-full rounded absolute shadow md:block hidden'></div>
             </div>
-            <div className='w-full h-20 bg-red-200 mt-10c'></div>
+            <div className='diagonalParent w-full h-80 flex flex-row-reverse shadow-lg rounded-lg relative mt-20'>
+                <div className='diagonal2 w-full h-full rounded absolute shadow md:block hidden'></div>
+                <div className='banner-text flex flex-col justify-evenly md:justify-start'>
+                  <p className="
+                    text-black font-bold
+                    md:mt-6 md:mr-6 lg:mt-8 lg:mr-8 xl:mt-10 xl:mr-10
+                    text-3xl md:text-4xl lg:text-5xl 2xl:text-7xl
+                    text-center md:text-right
+                  ">
+                    Build your own <span className='text-red-500'>decks</span> to play on tournaments!
+                  </p>
+                </div>
+            </div>
         </main>
     </ThemeProvider>
   );
